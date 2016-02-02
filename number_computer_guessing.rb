a0e15340e1c8776d4626ff0688f8f4911be22cb9
macGH <just_mac@hotmail.com>
2016-02-02 17:16:51 -0500

    In the midst of nightmare mode: unfortunately with error

--- number_computer_guessing.rb
+++ number_computer_guessing.rb
@@ -1,6 +1,8 @@
 rand_num = (1..100).to_a.sample
 
-puts "Shhh, rand_num is #{rand_num.to_s}\n\n" #
+puts "The random number is #{rand_num}"
+
+puts "Enter a number from 1 to 100 and the computer has five guesses to match it" #
 
 # validates that a number is in the valid integer range
 def is_valid_input(input)
@@ -8,14 +10,8 @@ def is_valid_input(input)
 end
 
 # gathers user input and validates it
-def gather_user_input
-  puts "Enter your guess from a number 1 through 100: "
-  user_input = gets.chomp
-  if is_valid_input (user_input)
-    return user_input.to_i
-  else
-    return 0      # return zero as marker for invalid input
-  end
+def gather_computer_guess (ary)
+  return ary.sample
 end
 
 # did the user follow the hint
@@ -27,37 +23,40 @@ def in_the_right_direction? (ary, hint)
   end
 end
 
-user_guesses = []  #
-user_guess = 0
-user_count = 0  #track number of guesses
+bucket = (1..100).to_a
+computer_guesses = []  #
+computer_guess = 0
+count = 0  #track number of guesses
 guess_max = 5   #max number of guess
 high_low = 0    #use -1 for lower, 0 for equal, 1 for higher
 
-
-until user_guess == rand_num || user_count == 5  #until correct guess or the max is hit
-  user_guess = gather_user_input
-  if user_guess == 0      #input was not valid
-    puts "Basic math -- You haven't entered an integer between 1 and 100. Try again. \n\n"
-
-  elsif (user_guesses.count > 0) && (user_guesses.count(user_guess) > 0)  # was this guess already provided?
-    puts "Short term memory issues? You've already guessed #{user_guess}.  Try again. \n\n"
-
-  elsif   #this is a valid guess
-    user_guesses << user_guess
-    in_the_right_direction?(user_guesses, high_low)
-    if user_guess < rand_num
-      puts "Pick a higher number: \n "
-      high_low = 1
-      user_count += 1
-    elsif user_guess > rand_num
-      puts "Pick a lower number: \n"
-      high_low = -1
-      user_count += 1
+loop do
+    if count == 5
+      break
     end
-  end
+    computer_guess = gather_computer_guess(bucket)
+    puts "computer guessed #{computer_guess}"
+    #this is a valid guess
+    computer_guesses << computer_guess
+    in_the_right_direction?(computer_guesses, high_low)
+    if computer_guess == rand_num
+      break
+    elsif  computer_guess < rand_num
+      puts "Pick a higher number: \n "
+      bucket = bucket[computer_guess..bucket.max]
+      puts bucket
+      high_low = 1
+      count += 1
+    elsif computer_guess > rand_num
+      puts "Pick a lower number: \n"
+      bucket = bucket[bucket.first..computer_guess-1]
+      puts bucket
+      high_low = -1
+      count += 1
+    end
 end
 
-if user_guess == rand_num
+if computer_guess == rand_num
   puts "You're a genius!! #{rand_num} is correct!"
 else
   puts "LOSER!! Maybe you would've guessed #{rand_num} with 100 guesses!"
